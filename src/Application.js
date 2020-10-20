9cff80c4795e4835d0f8f9f4a37985f71616c27a
Chdouluch <chedly.hafaiedh@gmail.com>
2020-10-20 15:22:45 +0200

    MapMarker

--- src/Application.js
+++ src/Application.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import {Services} from './http-services';
+import React from "react";
+import { Services } from "./http-services";
+import { CustomMap } from "./CustomMap";
+import "./stylesheets/main.scss";
 
-export class Application extends React.Component{
-    constructor(props){
-        super(props);
-        this.state= {
-            allCountries : []
-        };
-        console.log(props.children)
-    }
- 
+export class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allCountries: [],
+    };
+    console.log(props.children);
+  }
 
-    render(){
-        const Children = () => this.props.children;
-        return <div>
-            {JSON.stringify(this.state)}
-            
-            <Children/>
-            
-            </div>;
-    }
+  render() {
+    const Children = () => this.props.children;
+    const CountriesList = () =>
+      this.state.allCountries.map((countriy, index) => (
+        <div key={index}>{countriy.name}</div>
+      ));
 
-    componentDidMount(){
-        Services.getAllCountries().then((results)=>{
-            console.log(results.data)
-            this.setState({
-                allCountries: results.data
-            });
-        });
-       
-        
-    }
+    return <CustomMap allCountries={this.state.allCountries} />;
+  }
 
-
+  componentDidMount() {
+    Services.getAllCountries().then((results) => {
+      console.log(results.data);
+      this.setState({
+        allCountries: results.data,
+      });
+    });
+  }
 }
